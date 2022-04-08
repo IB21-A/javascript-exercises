1cc8b30273898215a737972d3968560c9f619dec
Thom Zolghadr <thom@thomcodes.com>
2022-04-08 16:37:40 -0700

    Pass hello world

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (str, numberOfRepeats) {
+  if (numberOfRepeats < 0) return "ERROR";
 
+  outputString = "";
+  for (let i = 0; i < numberOfRepeats; i++) {
+    outputString += str;
+  }
+
+  return outputString;
 };
 
 // Do not edit below this line

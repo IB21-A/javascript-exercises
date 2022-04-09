8863069b665c2d2f0545df7c5445b91e75f2d8cb
Thom Zolghadr <thom@thomcodes.com>
2022-04-08 18:51:43 -0700

    Pass test 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  if (!nums || nums.length === 0) return 0;
+
+  return nums.reduce((prev, current) => prev + current);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((prev, current) => prev * current);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +32,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

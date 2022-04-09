44cac38f27b79cc18b9330d2cad8f430c5e5d265
Thom Zolghadr <thom@thomcodes.com>
2022-04-08 17:38:24 -0700

    Pass test 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  return parseFloat(((temp - 32) * (5 / 9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  // [°F] = [°C] × 9⁄5 + 32
+  return parseFloat((temp * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };

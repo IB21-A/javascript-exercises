8bb8bb1fcf4fe0b6aefa02daeda693f328b54792
Thom Zolghadr <thom@thomcodes.com>
2022-04-08 19:14:43 -0700

    Pass test 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let filteredArray = str
+    .toLowerCase()
+    .split("")
+    .filter((c) => c.match(/[a-z]/g));
 
+  let forward = filteredArray.join("");
+  let reversed = filteredArray.reverse().join("");
+
+  return forward === reversed;
 };
 
 // Do not edit below this line

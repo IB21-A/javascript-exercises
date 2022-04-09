a2c20c2ac85907f50b630560aed2531e1995c056
Thom Zolghadr <thom@thomcodes.com>
2022-04-08 17:22:36 -0700

    Pass test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start === "string" ||
+    typeof end === "string"
+  ) {
+    return "ERROR";
+  }
 
+  if (start > end) {
+    let temp = end;
+    end = start;
+    start = temp;
+  }
+
+  try {
+    let range = Array.from({ length: end - start + 1 }, (_, i) => i + 1);
+    return range.reduce((prev, current) => prev + current + 0);
+  } catch (error) {
+    return "ERROR";
+  }
 };
+
+const swap = (a, b) => {};
 
 // Do not edit below this line
 module.exports = sumAll;

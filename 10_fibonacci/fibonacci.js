e8c38dc6a04d7481aace1fea0f720f62dfec4bc4
Thom Zolghadr <thom@thomcodes.com>
2022-04-11 16:56:16 -0700

    add iterative approach to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,20 @@
 const fibonacci = function (num) {
-  num = parseInt(num);
+  // num = parseInt(num);
   if (num < 1) return "OOPS";
-  if (num === 1) return 1;
-  if (num === 2) return 1;
+  // if (num === 1) return 1;
+  // if (num === 2) return 1;
 
-  return fibonacci(num - 1) + fibonacci(num - 2);
+  // return fibonacci(num - 1) + fibonacci(num - 2);
+
+  // Iterative approach
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= num; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
